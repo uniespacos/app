0266291d308faf014aef9e208ff8f2eb63453e89
Pedro Pinheiro <phplemos.dev@gmail.com>
2026-02-10 11:55:32 -0300

    fix(tests): remover espaço em branco desnecessário no método setUp e adicionar nova linha no final do arquivo ExampleTest

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -13,7 +13,7 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
+
         if (method_exists($this, 'withoutVite')) {
             $this->withoutVite();
         }
