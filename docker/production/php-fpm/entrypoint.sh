ff259a8ed241c0f774123b05ad2f617940d6bcfc
Pedro Pinheiro <phplemos.dev@gmail.com>
2026-02-11 16:37:10 -0300

    fix(ci): Resolve build failure and streamline deployment

--- docker/production/php-fpm/entrypoint.sh
+++ docker/production/php-fpm/entrypoint.sh
@@ -27,18 +27,5 @@ fi
 # Remove storage-init directory
 rm -rf /var/www/storage-init
 
-# Run Laravel migrations
-# -----------------------------------------------------------
-# Ensure the database schema is up to date.
-# -----------------------------------------------------------
-php artisan migrate --force
-
-# Clear and cache configurations
-# -----------------------------------------------------------
-# Improves performance by caching config and routes.
-# -----------------------------------------------------------
-php artisan config:cache
-php artisan route:cache
-
 # Run the default command
 exec "$@"
