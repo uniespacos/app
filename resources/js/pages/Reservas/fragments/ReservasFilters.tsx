73f5db523c854dd39d06f212dc0230c4685b00fa
Pedro Henrique Pinheiro Lemos <85008833+phplemos@users.noreply.github.com>
2026-02-19 10:35:03 -0300

    fix: set default locale to pt-BR for Calendar component (#97)

--- resources/js/pages/Reservas/fragments/ReservasFilters.tsx
+++ resources/js/pages/Reservas/fragments/ReservasFilters.tsx
@@ -64,7 +64,7 @@ export function ReservasFilters({
                         </Button>
                     </PopoverTrigger>
                     <PopoverContent className="w-auto p-0" align="start">
-                        <Calendar mode="single" selected={selectedDate} onSelect={onDateChange} locale={ptBR} initialFocus />
+                        <Calendar mode="single" selected={selectedDate} onSelect={onDateChange} initialFocus />
                     </PopoverContent>
                 </Popover>
             </div>
