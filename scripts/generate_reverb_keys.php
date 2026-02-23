aa9c624bf797891f88e472fab37faecaafeaf974
Pedro Pinheiro <phplemos.dev@gmail.com>
2026-02-23 08:45:06 -0300

    feat: add Reverb credentials generation script and update environment configurations

---
+++ scripts/generate_reverb_keys.php
@@ -0,0 +1,20 @@
+<?php
+
+function generateReverbCredentials()
+{
+    $appId = rand(100000, 999999);
+    $appKey = bin2hex(random_bytes(10));    // 20 characters
+    $appSecret = bin2hex(random_bytes(10)); // 20 characters
+
+    return [
+        'REVERB_APP_ID' => $appId,
+        'REVERB_APP_KEY' => $appKey,
+        'REVERB_APP_SECRET' => $appSecret,
+    ];
+}
+
+$creds = generateReverbCredentials();
+
+echo "REVERB_APP_ID=" . $creds['REVERB_APP_ID'] . PHP_EOL;
+echo "REVERB_APP_KEY=" . $creds['REVERB_APP_KEY'] . PHP_EOL;
+echo "REVERB_APP_SECRET=" . $creds['REVERB_APP_SECRET'] . PHP_EOL;
