2585f9c4c8c0e1c8eaea0b8fe02a61dfe95f8a29
phplemos <phplemos.dev@gmail.com>
2026-08-23 10:31:22 -0300

    feat: lazy-load quote prop in Inertia shared data

--- app/Http/Middleware/HandleInertiaRequests.php
+++ app/Http/Middleware/HandleInertiaRequests.php
@@ -39,12 +39,14 @@ class HandleInertiaRequests extends Middleware
      */
     public function share(Request $request): array
     {
-        [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
-
         return [
             ...parent::share($request),
             'name' => config('app.name'),
-            'quote' => ['message' => trim($message), 'author' => trim($author)],
+            'quote' => function () {
+                [$message, $author] = str(Inspiring::quotes()->random())->explode('-');
+
+                return ['message' => trim($message), 'author' => trim($author)];
+            },
             'auth' => [
                 'user' => fn () => $request->user() ? $this->formatAuthUser($request->user()) : null,
             ],
