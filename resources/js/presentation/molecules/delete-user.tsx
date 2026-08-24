ee17e4faa06acdb92de1d51a6010a5853c6075e7
phplemos <phplemos.dev@gmail.com>
2026-08-23 23:18:49 -0300

    fix: abertura direta do modal de confirmacao de exclusao com validacao de senha

--- resources/js/presentation/molecules/delete-user.tsx
+++ resources/js/presentation/molecules/delete-user.tsx
@@ -1,5 +1,5 @@
 import DeleteItem from '@/presentation/molecules/delete-item';
 
 export default function DeleteUser() {
-    return <DeleteItem itemName="conta" route={route('settings.profile.destroy')} showHeading={true} />;
+    return <DeleteItem itemName="conta" route={route('settings.profile.destroy')} showHeading={true} variant="card" />;
 }
